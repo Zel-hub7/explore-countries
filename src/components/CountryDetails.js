8cd4f318a9294a6235c5005f89a64cb54e3240df
zel-hub7 <zelalemyohannes62@gmail.com>
2023-09-08 08:20:33 -0700

    update the countryList

--- src/components/CountryDetails.js
+++ src/components/CountryDetails.js
@@ -11,74 +11,68 @@ const CountryDetails = () => {
   const details = data.filter((country) => country.countryID === countryId)[0];
   const navigation = useNavigate();
 
-  console.log("details:", details);
-
-  if (!details) {
-    return (
-      <div>
-        <p className="not-found">Country not found</p>
-        <button
-          type="button"
-          onClick={() => navigation(-1)}
-          className="back-btn"
-        >
-          <BiArrowBack />
-        </button>
-      </div>
-    );
-  }
-
   return (
     <div className="countryDetails">
-      <div className="detail-nav">
-      <button
-          type="button"
-          onClick={() => navigation(-1)}
-          className="back-btn"
-        >
-          <BiArrowBack />
-        </button>
-        <h2>{details.name}</h2>
-        <div className="btns">
-          <BiMicrophone data-testid="microphone-button" />{" "}
-          <AiFillSetting data-testid="settings-button" />{" "}
+      {details ? (
+        <>
+          <div className="detail-nav">
+            <button
+              type="button"
+              onClick={() => navigation(-1)}
+              className="back-btn"
+            >
+              <BiArrowBack />
+            </button>
+            <h2>{details.name}</h2>
+            <div className="btns">
+              <BiMicrophone data-testid="microphone-button" />{" "}
+              <AiFillSetting data-testid="settings-button" />{" "}
+            </div>
+          </div>
+
+          <div className="flag-container">
+            <img src={details.flag.png} alt={details.name} />
+            <p>{details.flag.alt}</p>
+          </div>
+
+          <div className="more-details">
+            <h2>{details.officialName}</h2>
+            <div className="details-list">
+              <h3>Capital City</h3>
+              <h3>{details.capital[0]}</h3>
+            </div>
+            <div className="details-list">
+              <h3>Region</h3>
+              <h3>{details.region}</h3>
+            </div>
+            <div className="details-list">
+              <h3>Start of Week</h3>
+              <h3>{details.startOfWeek}</h3>
+            </div>
+            <div className="details-list">
+              <h3>Driving Side</h3>
+              <h3>{details.drivingSide}</h3>
+            </div>
+            <div className="details-list">
+              <h3>Continent</h3>
+              <h3>{details.continent[0]}</h3>
+            </div>
+          </div>
+        </>
+      ) : (
+        <div>
+          <p className="not-found">Country not found</p>
+          <button
+            type="button"
+            onClick={() => navigation(-1)}
+            className="back-btn"
+          >
+            <BiArrowBack />
+          </button>
         </div>
-      </div>
-
-      <div className="flag-container">
-        <img src={details.flag.png} alt={details.name} />
-        <p>{details.flag.alt}</p>
-      </div>
-
-      <div className="more-details">
-        <h2>{details.officialName}</h2>
-        <div className="details-list">
-          <h3>Capital City</h3>
-          <h3>{details.capital[0]}</h3>
-        </div>
-
-
-        <div className="details-list">
-          <h3>Region</h3>
-          <h3>{details.region}</h3>
-        </div>
-
-        <div className="details-list">
-          <h3>Start of Week</h3>
-          <h3>{details.startOfWeek}</h3>
-        </div> 
-
-        <div className="details-list">
-          <h3>Driving Side</h3>
-          <h3>{details.drivingSide}</h3>
-        </div>
-        <div className="details-list">
-          <h3>Continent</h3>
-          <h3>{details.continent[0]}</h3>
-        </div>
- 
-      </div>
+      )}
     </div>
   );
 };
+
 export default CountryDetails;
